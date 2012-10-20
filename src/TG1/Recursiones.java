a496316869c1396be5477b13a7a856b3fde2e680
Carlos Gochez <carlos.gochez@unitec.edu>
2012-10-19 19:23:41 -0600

    commit inicial e incluir la pauta del TG1

---
+++ src/TG1/Recursiones.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TG1;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Recursiones {
+
+    private int array[] = { 3,5,6,8,1 };
+    
+    //Prueba para las 6:40 y ejercicio de TG1 para 6:45
+    public static boolean buscarEnCadena(String cad,char carac){
+        return buscarEnCadena(cad, carac, 0);
+    }
+    
+    private static boolean buscarEnCadena(String cad, char carac, int pos) {
+        if( pos < cad.length() ){
+            if( cad.charAt(pos) == carac )
+                return true;
+            return buscarEnCadena(cad, carac, pos +1 );
+        }
+        return false;
+    }
+    
+    public void imprimir(){
+        imprimir(0);
+        System.out.println("");
+    }
+    
+    private void imprimir(int pos){
+        if( pos < array.length ){
+            System.out.print( array[pos] + " ");
+            imprimir(pos+1);
+        }
+    }
+    
+    public int suma(){
+        return suma(0);
+    }
+
+    private int suma(int pos) {
+        if( pos < array.length )
+            return array[pos] + suma(pos+1);
+        return 0;
+    }
+    
+    public int mayor(){
+        return mayor(0, array[0]);
+    }
+
+    private int mayor(int pos, int grande) {
+        if(pos < array.length){
+            if( array[pos] >  grande )
+                grande = array[pos];
+            return mayor(pos+1, grande);
+        }
+        return grande;
+    }
+    
+    public int divisibles(int divi){
+        return divisibles(divi,0);
+    }
+
+    private int divisibles(int divi, int pos) {
+        if( pos < array.length ){
+            if( array[pos] % divi == 0 )
+                return 1 + divisibles(divi,pos+1);
+            return divisibles(divi,pos+1);
+        }
+        return 0;
+    }
+}
---
+++ src/TG1/TestTG1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TG1;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class TestTG1 {
+    public static void main(String args[]){
+        Recursiones rec = new Recursiones();
+        
+        rec.imprimir();
+        
+        System.out.println("\nSuma: " + rec.suma() );
+        System.out.println("\nMayor: " + rec.mayor());
+        System.out.println("\nCuantos divisibles pares: " + rec.divisibles(2));
+        
+        //Cadena
+        String cad = "FC Barcelona";
+        if( Recursiones.buscarEnCadena(cad, 'F'))
+            System.out.println("Se encontro F");
+        else
+            System.out.println("No se encontro F");
+    }
+}
