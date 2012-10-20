8539a7445e883bd17c208e9303e220d4d076df1d
Carlos Gochez <carlos.gochez@unitec.edu>
2012-10-19 20:28:17 -0600

    Clase de Prueba

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Solo de Prueba");
+    }
+}
